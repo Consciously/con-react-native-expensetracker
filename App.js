ac816d3fdbd7f58b30c5fc4fce40cfdd0381245c
Stefan Ihle <stefanihle@gmx.net>
2022-05-18 22:12:34 +0200

    solve text component issue

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import { StatusBar } from 'expo-status-bar';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
 import ManageExpense from './screens/ManageExpense';
 import RecentExpenses from './screens/RecentExpenses';
 import AllExpenses from './screens/AllExpenses';
@@ -21,7 +22,7 @@ const ExpensesOverview = () => {
 export default function App() {
 	return (
 		<>
-			<StatusBar style='auto' />;
+			<StatusBar style='auto' />
 			<NavigationContainer>
 				<Stack.Navigator>
 					<Stack.Screen name='ExpensesOverview' component={ExpensesOverview} />
