b105f92fc069ea2775df98a9ccd44f9b43de2c96
Stefan Ihle <stefanihle@gmx.net>
2022-05-25 15:30:04 +0200

    Add custom button component and style them

--- screens/ManageExpense.js
+++ screens/ManageExpense.js
@@ -1,5 +1,6 @@
 import { useLayoutEffect } from 'react';
 import { View, StyleSheet } from 'react-native';
+import Button from '../components/UI/Button';
 import IconButton from '../components/UI/IconButton';
 import { GlobalStyles } from '../contstants/styles';
 
@@ -14,19 +15,29 @@ const ManageExpense = ({ route, navigation }) => {
 	}, [navigation, isEditing]);
 
 	const deleteExpenseHandler = () => {};
+	const cancelHandler = () => {};
+	const confirmHandler = () => {};
 
 	return (
 		<View style={styles.container}>
-			<View style={styles.deleteContainer}>
-				{isEditing && (
+			<View style={styles.buttons}>
+				<Button style={styles.button} mode='flat' onPress={cancelHandler}>
+					Cancel
+				</Button>
+				<Button style={styles.button} onPress={confirmHandler}>
+					{isEditing ? 'Update' : 'Add'}
+				</Button>
+			</View>
+			{isEditing && (
+				<View style={styles.deleteContainer}>
 					<IconButton
 						icon='trash'
 						size={36}
 						color={GlobalStyles.colors.error500}
 						onPress={deleteExpenseHandler}
 					/>
-				)}
-			</View>
+				</View>
+			)}
 		</View>
 	);
 };
@@ -38,6 +49,15 @@ const styles = StyleSheet.create({
 		flex: 1,
 		padding: 24,
 		backgroundColor: GlobalStyles.colors.primary800
+	},
+	buttons: {
+		flexDirection: 'row',
+		justifyContent: 'center',
+		alignItems: 'center'
+	},
+	button: {
+		minWidth: 120,
+		marginHorizontal: 8
 	},
 	deleteContainer: {
 		marginTop: 16,
