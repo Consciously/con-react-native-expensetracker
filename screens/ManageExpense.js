0869e84603cb92c2edb9bb98b7e94fc8dd9eed27
Stefan Ihle <stefanihle@gmx.net>
2022-05-25 11:55:02 +0200

    conditionally insert IconButton component to ManageExpense screen and style them

--- screens/ManageExpense.js
+++ screens/ManageExpense.js
@@ -1,5 +1,7 @@
 import { useLayoutEffect } from 'react';
-import { Text } from 'react-native';
+import { View, StyleSheet } from 'react-native';
+import IconButton from '../components/UI/IconButton';
+import { GlobalStyles } from '../contstants/styles';
 
 const ManageExpense = ({ route, navigation }) => {
 	const editedExpenseId = route.params?.expenseId;
@@ -11,7 +13,37 @@ const ManageExpense = ({ route, navigation }) => {
 		});
 	}, [navigation, isEditing]);
 
-	return <Text>Manage Expenses Screen</Text>;
+	const deleteExpenseHandler = () => {};
+
+	return (
+		<View style={styles.container}>
+			<View style={styles.deleteContainer}>
+				{isEditing && (
+					<IconButton
+						icon='trash'
+						size={36}
+						color={GlobalStyles.colors.error500}
+						onPress={deleteExpenseHandler}
+					/>
+				)}
+			</View>
+		</View>
+	);
 };
 
 export default ManageExpense;
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		padding: 24,
+		backgroundColor: GlobalStyles.colors.primary800
+	},
+	deleteContainer: {
+		marginTop: 16,
+		paddingTop: 8,
+		borderTopWidth: 2,
+		borderTopColor: GlobalStyles.colors.primary200,
+		alignItems: 'center'
+	}
+});
